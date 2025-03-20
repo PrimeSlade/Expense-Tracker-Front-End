ef7123698399b8eedd2b7220c7ea75342f3ce448
Slade <saib6064@gmail.com>
2025-03-20 16:21:21 +0700

    App: protecting routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,19 @@ import { useAuthContext } from "./hook/useAuthConext";
 
 function App() {
   const { user } = useAuthContext();
-  if (user) {
-    console.log(user.id);
-  }
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+        <Route
+          path="/signup"
+          element={!user ? <Signup /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/" />}
+        />
       </Routes>
     </>
   );
