de1995b086a734042433a90200ab11a21419916e
Slade <saib6064@gmail.com>
2025-04-01 20:08:52 +0700

    feat(hook): implement axios

--- src/hook/useLogin.jsx
+++ src/hook/useLogin.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useAuthContext } from "./useAuthConext";
 import { useNavigate } from "react-router";
+import axiosInstance from "../../axiosInstance";
 
 export const useLogin = () => {
   const [error, setError] = useState(null);
@@ -14,28 +15,28 @@ export const useLogin = () => {
     setIsLoading(true);
     setError(null);
 
-    //fetch
-    const res = await fetch("http://localhost:3000/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email: email, password: password }),
-      credentials: "include", //for storing cookie that is sent from backends server
-    });
+    const { data } = await axiosInstance.post(
+      "/login",
+      {
+        email: email,
+        password: password,
+      },
+      { headers: { "Content-Type": "application/json" } }
+    );
 
-    const user = await res.json();
-    console.log(user);
+    console.log(data);
 
     //not ok
-    if (user.error) {
+    if (data.error) {
       setIsLoading(false);
-      setError(user.error);
+      setError(data.error);
     }
-    if (user.id) {
+    if (data.id) {
       //save the user to local storage
-      localStorage.setItem("user", JSON.stringify(user));
+      localStorage.setItem("user", JSON.stringify(data));
 
       //update the auth context
-      dispatch({ type: "LOGIN", payload: user });
+      dispatch({ type: "LOGIN", payload: data });
       setIsLoading(false);
       navigate("/");
     }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,8 @@
+import { useAuthContext } from "../../hook/useAuthConext";
+
 const Home = () => {
+  const { dispatch } = useAuthContext();
+
   const data = async () => {
     try {
       const res = await fetch("http://localhost:3000/create", {
@@ -7,6 +11,12 @@ const Home = () => {
       });
       const data = await res.json();
       console.log(data);
+
+      if (data.error) {
+        //VERY IMPORTANT need to do with alert box for redirecting user to login page
+        localStorage.removeItem("user");
+        dispatch({ type: "LOGOUT" });
+      }
     } catch (err) {
       console.log(err);
     }
