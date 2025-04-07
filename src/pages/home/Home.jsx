1b5b9f3d6ad04f24371b6bdeea9519204026be62
Slade <saib6064@gmail.com>
2025-04-07 10:31:20 +0700

    feat(pages): implement carousel

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,10 +2,22 @@ import DoughnutChart from "@/components/charts/DoughnutChart";
 import PolarAreaChart from "@/components/charts/PolarAreaChart";
 import VerticalBarChart from "@/components/charts/VerticalBarChart";
 import { Button } from "@/components/ui/button";
-import React from "react";
+import { ActiveContext } from "@/context/ActiveContext";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 const Home = () => {
-  // Rader, polar, vertical bar chart
+  const { setActive } = useContext(ActiveContext);
+
+  const navigate = useNavigate();
+
   return (
     <>
       <div className="flex justify-around mt-10">
@@ -15,14 +27,22 @@ const Home = () => {
             track is a step toward financial{" "}
             <span className="text-[var(--primary-color)]">freedom</span>.
           </h2>
-          <Button className={"mt-5"}>+ Add Expense List</Button>
+          <Button
+            className={"mt-5"}
+            onClick={() => {
+              setActive("Expenses");
+              navigate("/expenses");
+            }}
+          >
+            + Add Expense List
+          </Button>
         </div>
         <div className="w-120">
           <VerticalBarChart />
         </div>
       </div>
       <div className="flex justify-around mt-20 gap-5">
-        <div className=" border h-50 bg-white shadow rounded-2xl min-w-80 pl-5 flex flex-col justify-center gap-3">
+        {/* <div className=" border h-50 bg-white shadow rounded-2xl min-w-80 pl-5 flex flex-col justify-center gap-3">
           <div>
             <h2 className="text-gray-500">Income</h2>
             <h2 className="font-bold text-[var(--primary-color)] text-2xl">
@@ -33,7 +53,41 @@ const Home = () => {
             <h2 className="text-gray-500">Expense</h2>
             <h2 className="font-bold text-red-600 text-2xl">2000k</h2>
           </div>
-        </div>
+        </div> */}
+        <Carousel
+          opts={{
+            align: "start",
+          }}
+          orientation="vertical"
+          className="w-full max-w-xs h-50 flex justify-center flex-col"
+        >
+          <CarouselContent className="-mt-1 h-[200px] gap-1">
+            <CarouselItem className="md:basis-1/2">
+              <div className="flex items-center flex-col border-white p-3 rounded-2xl shadow bg-white">
+                <h2 className="text-gray-500">Income</h2>
+                <h2 className="font-bold text-[var(--primary-color)] text-2xl">
+                  1000k
+                </h2>
+              </div>
+            </CarouselItem>
+
+            <CarouselItem className="pt-1 md:basis-1/2">
+              <div className="flex items-center flex-col border-white p-3 rounded-2xl shadow bg-white">
+                <h2 className="text-gray-500">Expense</h2>
+                <h2 className="font-bold text-red-600 text-2xl">100k</h2>
+              </div>
+            </CarouselItem>
+            <CarouselItem className="pt-1 md:basis-1/2">
+              <div className="flex items-center flex-col border-white p-3 rounded-2xl shadow bg-white">
+                <h2 className="text-gray-500">Debt</h2>
+                <h2 className="font-bold text-2xl">0k</h2>
+              </div>
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+
         <div className="flex flex-row">
           <PolarAreaChart />
           <DoughnutChart />
