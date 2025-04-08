3f59aef44e7814dc64b8f344609c7c2cb72c5cc3
Slade <saib6064@gmail.com>
2025-04-08 11:43:25 +0700

    feat(page): add notes

--- src/pages/expenses/Expenses.jsx
+++ src/pages/expenses/Expenses.jsx
@@ -12,9 +12,14 @@ const Expenses = () => {
   const { user } = useAuthContext();
   const [isHidden, setIsHidden] = useState(true);
   const [datas, setDatas] = useState();
-  console.log(datas);
-
+  const [activeId, setActiveId] = useState(null);
   const { fetch, error } = useFetchData();
+
+  const onActive = (id) => {
+    setActiveId(activeId === null ? id : null);
+  };
+
+  console.log(activeId);
 
   useEffect(() => {
     const getData = async () => {
@@ -47,20 +52,50 @@ const Expenses = () => {
             return (
               <div id={d.id} key={i} className="text-xl">
                 <div className="flex justify-center">
-                  <div className="border flex justify-around p-10 mt-5 w-230 rounded-xl bg-white">
-                    <div className="flex gap-3">
-                      <div>
-                        <FontAwesomeIcon icon={faUser} />
+                  <div className="border grid grid-cols-1 p-10 mt-5 w-230 rounded-xl bg-white">
+                    <div
+                      className="flex justify-around"
+                      onClick={() => onActive(d.id)}
+                    >
+                      <div className="flex gap-3">
+                        <div>
+                          <FontAwesomeIcon icon={faUser} />
+                        </div>
+                        <h2>{d.categories}</h2>
                       </div>
-                      <h2>{d.categories}</h2>
+                      <div className="font-bold">
+                        <span
+                          className={
+                            d.transaction_type === "Income"
+                              ? "text-[var(--primary-color)]"
+                              : "text-red-600"
+                          }
+                        >
+                          {d.cost}
+                        </span>{" "}
+                        <span>{user.currency}</span>
+                      </div>
+                      <Badge variant="outline" className={"font-bold w-30"}>
+                        {d.transaction_type}
+                      </Badge>
+                      <div>{d.created_at}</div>
                     </div>
-                    <div>
-                      {d.cost} {user.currency}
+                    {/* I used Ai for animation.Ofc,I am not really good at making animation :> */}
+                    <div
+                      className={` text-md transition-all duration-500 ease-out transform ${
+                        activeId !== d.id
+                          ? "opacity-0 scale-95 max-h-0 overflow-hidden "
+                          : "opacity-100 scale-100 max-h-[500px] mt-5"
+                      }`}
+                    >
+                      <div>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing
+                        elit. Laborum ipsam repudiandae nihil maxime voluptatem
+                        quae blanditiis voluptatibus earum, provident ab hic
+                        quod, saepe quisquam eligendi! Esse exercitationem ab
+                        nisi molestiae.
+                      </div>
                     </div>
-                    <Badge variant="outline" className={"font-bold w-30"}>
-                      {d.transaction_type}
-                    </Badge>
-                    <div>{d.created_at}</div>
                   </div>
                 </div>
               </div>
