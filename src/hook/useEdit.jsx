ec4a221486121ae4b27c7c98097f11eb08f7f5f9
Slade <saib6064@gmail.com>
2025-04-10 20:59:07 +0700

    feat(hook): implement useEdit

---
+++ src/hook/useEdit.jsx
@@ -0,0 +1,37 @@
+import axiosInstance from "../../axiosInstance";
+import React, { useState } from "react";
+
+export const useEdit = () => {
+  const [editError, setEditError] = useState(null);
+
+  const edit = async (
+    id,
+    category,
+    note,
+    created_at,
+    cost,
+    transaction_type
+  ) => {
+    try {
+      const { data } = await axiosInstance.put(
+        `/lists/${id}`,
+        {
+          category: category,
+          note: note,
+          created_at: created_at,
+          cost: cost,
+          transaction_type: transaction_type,
+        },
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+
+      return data;
+    } catch (error) {
+      console.log(error);
+      setEditError(error.response.data.error);
+    }
+  };
+  return { edit, editError, setEditError };
+};
